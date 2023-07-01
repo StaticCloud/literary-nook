295aa78e0367578acecb3f19a475cb0755487025
StaticCloud <diegomarrs@gmail.com>
2023-07-01 16:07:24 -0700

    basic file structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <header>
+            <h1>RETRO CRAWLER</h1>
+            <nav>
+                <ul>HOME</ul>
+                <ul>ABOUT</ul>
+                <ul>CONTACT</ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
