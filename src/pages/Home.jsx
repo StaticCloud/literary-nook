45c2afc55bdb364a6fa4ba893908d1acb5c9d333
StaticCloud <diegomarrs@gmail.com>
2023-07-03 14:40:59 -0700

    improvements to landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,20 @@
+import styled from 'styled-components'
+
+const HomeWrapper = styled.section`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
 export default function Home() {
     return (
-        <h1>HOME</h1>
-
+        <HomeWrapper>
+            <div>
+                <h1>A PLACE FOR ALL YOUR RETRO GAMING NEEDS</h1>
+                <h2>Consoles, games, accessories, and even merch.</h2>
+            </div>
+        </HomeWrapper>
     );
 }
