bce0d7873ba504bcaa7e0a6f01e7857ce4df2a09
DMarrs <diegomarrs@gmail.com>
2023-07-05 21:44:08 -0700

    added background to landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,27 @@
+import interior from '../assets/img/interior.jpg';
+import styled from 'styled-components'
+
+const Background = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    background-size: cover;
+    background-position: center;
+    background-image: url(${interior});
+
+    &:after {
+        content: ' ';
+        display: block;
+        position: absolute;
+        z-index: 1;
+        background-color: #00000077;
+        width: 100%;
+        height: 100%;
+    }
+`;
+
 export default function Home() {
     return (
-        <h1>HOME</h1>
-
+        <Background/>
     );
 }
